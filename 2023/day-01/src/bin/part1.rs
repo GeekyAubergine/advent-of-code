abbd7721b749d53453fdaa3367343d6e9fb85610
Zoe Aubert <hello@zoeaubert.me>
2023-12-01 12:26:03 +0000

    Make clippy happy

--- 2023/day-01/src/bin/part1.rs
+++ 2023/day-01/src/bin/part1.rs
@@ -3,12 +3,6 @@ use miette::Context;
 
 #[tracing::instrument]
 fn main() -> miette::Result<()> {
-    #[cfg(feature = "dhat-heap")]
-    let _profiler = dhat::Profiler::new_heap();
-
-    #[cfg(not(feature = "dhat-heap"))]
-    tracing_subscriber::fmt::init();
-
     let file = include_str!("../../input1.txt");
     let result = process(file).context("process part 1")?;
     println!("{}", result);
--- 2023/day-01/src/bin/part2_opt.rs
+++ 2023/day-01/src/bin/part2_opt.rs
@@ -3,12 +3,6 @@ use miette::Context;
 
 #[tracing::instrument]
 fn main() -> miette::Result<()> {
-    #[cfg(feature = "dhat-heap")]
-    let _profiler = dhat::Profiler::new_heap();
-
-    #[cfg(not(feature = "dhat-heap"))]
-    tracing_subscriber::fmt::init();
-
     let file = include_str!("../../input2.txt");
     let result = process(file).context("process part 2")?;
     println!("{}", result);
