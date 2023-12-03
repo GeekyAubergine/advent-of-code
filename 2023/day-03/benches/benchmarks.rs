ad9898a171818663db24f8ba12e902ed6a320ea4
Zoe Aubert <hello@zoeaubert.me>
2023-12-03 14:31:49 +0000

    benchmarks

--- 2023/day-03/benches/benchmarks.rs
+++ 2023/day-03/benches/benchmarks.rs
@@ -13,13 +13,13 @@ fn part1() {
     .unwrap();
 }
 
-// #[divan::bench]
-// fn part2() {
-//     part2::process(divan::black_box(include_str!(
-//         "../input2.txt",
-//     )))
-//     .unwrap();
-// }
+#[divan::bench]
+fn part2() {
+    part2::process(divan::black_box(include_str!(
+        "../input2.txt",
+    )))
+    .unwrap();
+}
 
 // #[divan::bench]
 // fn part1_opt() {
