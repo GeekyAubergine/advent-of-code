423001bb5fbf45d656cfc2af9a3c57def9803aaa
Zoe Aubert <hello@zoeaubert.me>
2023-12-09 10:02:36 +0000

    part1

---
+++ 2023/day-09/src/error.rs
@@ -0,0 +1,14 @@
+use miette::Diagnostic;
+use thiserror::Error;
+
+#[derive(Error, Diagnostic, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
+    #[error("Could not parse number {0}")]
+    CouldNotParseNumber(#[from] std::num::ParseIntError),
+    #[error("Could not get bottom row of values")]
+    CouldNotGetBottomRowOfValues,
+    #[error("Could not get last value of row {0}")]
+    CouldNotGetLastValueOfRow(usize),
+}
